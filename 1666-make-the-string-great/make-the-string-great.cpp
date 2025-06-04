d5009c0f88dfe9994e85266d8abd9de694ee324a
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-06-04 10:09:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (54.75%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        int n = s.size();
+        string res = "";
+        for (int i = 0; i < n; i++) {
+            if (!res.empty() &&
+                (res.back() == s[i] + 32 || res.back() == s[i] - 32)) {
+                res.pop_back();
+            } else {
+                res.push_back(s[i]);
+            }
+        }
+        return res;
+    }
+};
