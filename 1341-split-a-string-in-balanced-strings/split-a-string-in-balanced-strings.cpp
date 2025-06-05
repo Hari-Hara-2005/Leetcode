3da749a70befa4c585491f579f2f0ae30d9f1e7c
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-06-05 11:07:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (83.15%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cpp
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int balancedStringSplit(string s) {
-        int leftSum=0;
-        int rightSum=0;
-        int n=s.size();
+        int x=0;
         int cnt=0;
+        int n=s.size();
         for(int i=0;i<n;i++)
         {
-            if(s[i]=='R')
-            {
-                rightSum++;
-            }
-            else{
-                leftSum++;
-            }
-            if(rightSum==leftSum)cnt++;
+            if(s[i]=='R')x++;
+            else if(s[i]=='L')x--;
+            if(x==0)cnt++;
         }
         return cnt;
     }
