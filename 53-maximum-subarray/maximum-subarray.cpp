7b791e74c2975e2e3b014f5c11cc5a81f9d1b05c
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-06-20 08:52:08 +0530

    Time: 0 ms (100.00%) | Memory: 71.9 MB (17.07%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,7 +4,8 @@ public:
         int n = nums.size();
         int maxi = nums[0];
         int curSum=nums[0];
-        for (int i = 1; i < n; i++) {
+        for(int i=1;i<n;i++)
+        {
            curSum=max(nums[i],curSum+nums[i]);
            maxi=max(maxi,curSum);
         }
