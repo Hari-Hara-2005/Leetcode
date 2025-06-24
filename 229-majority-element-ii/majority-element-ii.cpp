1650563090653869aa99f7afae9fcc51b7d4e542
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-06-24 21:20:09 +0530

    Time: 15 ms (11.42%) | Memory: 26.9 MB (9.46%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        map<int,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[nums[i]]++;
+        }
+        vector<int>ans;
+        for(auto nums:mp)
+        {
+            if(nums.second>n/3)
+            {
+                ans.push_back(nums.first);
+            }
+        }
+        return ans;
+    }
+};
