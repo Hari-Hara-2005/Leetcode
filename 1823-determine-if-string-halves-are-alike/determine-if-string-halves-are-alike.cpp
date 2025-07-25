8c6264bce03018c1509ac99f8ced495ad6aae02c
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-07-25 19:41:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (77.51%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
+    bool vowels(char ch){
+          if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' ||
+                ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' ||
+                ch == 'O' || ch == 'U') {
+                    return true;
+                }
+                return false;
+    }
     bool halvesAreAlike(string s) {
         int n=s.size();
-        vector<int> a;
-        vector<int> b;
         int cnt1 = 0, cnt2 = 0;
         for (int i = 0; i < n / 2; i++) {
-            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
-                s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' ||
-                s[i] == 'O' || s[i] == 'U') {
-                cnt1++;
-            }
-            a.push_back(s[i]);
-        }
-        for (int i = n / 2; i < n; i++) {
-            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
-                s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' ||
-                s[i] == 'O' || s[i] == 'U') {
-                cnt2++;
-            }
-            b.push_back(s[i]);
+         if(vowels(s[i]))
+         {
+            cnt1++;
+         }
+         if(vowels(s[n/2+i]))
+         {
+            cnt2++;
+         }
         }
         if(cnt1==cnt2)return true;
         return false;
