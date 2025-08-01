038ee72e3c1a532e71a6f8a88ce5a7a932fb315b
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-08-01 21:58:42 +0530

    Time: 0 ms (100.00%) | Memory: 33.2 MB (28.89%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
-        vector<int> ans;
         int n = words.size();
+        vector<int> ans;
         for (int i = 0; i < n; i++) {
-            string word = words[i];
-            for (int j = 0; j < word.size(); j++) {
-                if (word[j] == x) {
+            for (int j = 0; j < words[i].size(); j++) {
+                if (words[i][j] == x) {
                     ans.push_back(i);
                     break;
                 }
