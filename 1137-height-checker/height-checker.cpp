ff4aa31a5f1bca4ffe793f98f75305cabe2b7418
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-08-01 20:15:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (56.19%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        int cnt =0;
-        int n=heights.size();
-        vector<int>ans;
-        for(int i=0;i<n;i++)
-        {
-            ans.push_back(heights[i]);
-        }
-        sort(ans.begin(),ans.end());
-        for(int j=0;j<n;j++)
-        {
-            if(heights[j]!=ans[j])
-            {
+        int cnt = 0;
+        int n = heights.size();
+        vector<int> ans=heights;
+        sort(ans.begin(), ans.end());
+        for (int j = 0; j < n; j++) {
+            if (heights[j] != ans[j]) {
                 cnt++;
             }
         }
