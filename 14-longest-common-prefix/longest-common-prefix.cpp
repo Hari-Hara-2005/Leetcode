19942dcd4579f4ba16399f10beaa10885919fc7f
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-09-19 21:55:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (32.48%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,15 +3,12 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         sort(strs.begin(),strs.end());
         int n = strs.size();
-        string word1=strs[0];
-        string word2=strs[n-1];
-        string res="";
+        string word1 = strs[0];
+        string word2 = strs[n-1];
+        string res = "";
         for(int i=0;i<min(word1.size(),word2.size());i++)
         {
-            if(word1[i]!=word2[i])
-            {
-               return res;
-            }
+            if(word1[i]!=word2[i])return  res;
             res+=word1[i];
         }
         return strs[0];
