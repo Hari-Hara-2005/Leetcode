e9853acb26395340c2c24c144c7945d935df2e9e
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-11-04 20:24:53 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (55.20%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,22 +2,12 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        vector<int>ans;
-        int cnt=0;
+        int idx=0;
         for(int i=0;i<n;i++)
         {
-            if(nums[i]!=0)
-            {
-                ans.push_back(nums[i]);
-            }
-            else{
-                cnt++;
-            }
+            if(nums[i]!=0){
+                swap(nums[idx++],nums[i]);
+            } 
         }
-        for(int i=0;i<cnt;i++)
-        {
-            ans.push_back(0);
-        }
-        nums = ans;
     }
 };
