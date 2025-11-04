7f74f5b7e92eb043093d0e8708793219266430db
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-11-04 20:17:06 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (83.24%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int>ans;
-        int n = nums.size();
-        k = k%n;
-        for(int i=n-k;i<n;i++)
-        {
-            ans.push_back(nums[i]);
-        }
-        for(int i=0;i<nums.size()-k;i++)
-        {
-            ans.push_back(nums[i]);
-        }
-        nums = ans;
+        k = k%nums.size();
+       reverse(nums.begin(),nums.end());
+       reverse(nums.begin(),nums.begin()+k);
+       reverse(nums.begin()+k,nums.end());
     }
 };
