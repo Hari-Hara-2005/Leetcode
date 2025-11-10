4a9ae5d0f2ad3d30bec814706f5d929b2e351ca3
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-11-10 19:15:20 +0530

    Time: 50 ms (28.91%) | Memory: 75.7 MB (32.93%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,22 +10,49 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
+    ListNode* findMid(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+        while (l1 != nullptr && l2 != nullptr) {
+            if (l1->val < l2->val) {
+                temp->next = l1;
+                l1 = l1->next;
+            } else {
+                temp->next = l2;
+                l2 = l2->next;
+            }
+            temp = temp->next;
+        }
+        if (l1 != nullptr)
+            temp->next = l1;
+        else
+            temp->next = l2;
+        return dummyNode->next;
+    }
+
+    ListNode* ms(ListNode* head) {
         if (head == nullptr || head->next == nullptr)
             return head;
+        ListNode* mid = findMid(head);
+        ListNode* rightNode = mid->next;
+        mid->next = nullptr;
+        ListNode* leftNode = head;
+        leftNode = ms(leftNode);
+        rightNode = ms(rightNode);
+        return merge(leftNode, rightNode);
+    }
+    ListNode* sortList(ListNode* head) {
         ListNode* curr = head;
-        vector<int> arr;
-        while (curr) {
-            arr.push_back(curr->val);
-            curr = curr->next;
-        }
-        sort(arr.begin(), arr.end());
-        curr = head;
-        int idx = 0;
-        while (curr) {
-            curr->val = arr[idx++];
-            curr = curr->next;
-        }
-        return head;
+        return ms(head);
     }
 };
