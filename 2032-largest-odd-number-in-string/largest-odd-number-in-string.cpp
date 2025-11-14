2411e95e1ddfb62f143d7b7dea498fe368695be2
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-11-14 21:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (94.48%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -3,9 +3,13 @@ public:
     string largestOddNumber(string num) {
         int n = num.size();
         for (int i = n - 1; i >= 0; i--) {
-            int nu = num[i] - '0';
-            if (nu % 2 == 1)
-                return num.substr(0, i + 1);
+            int digit = num[i] - '0';
+            if (digit % 2 == 1)
+                return num;
+            else {
+                num.pop_back();
+                ;
+            }
         }
         return "";
     }
