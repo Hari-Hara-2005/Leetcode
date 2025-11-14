01510010f42d7f95f65f717b2a12a800ca95d14d
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-11-14 20:56:08 +0530

    Time: 12 ms (9.00%) | Memory: 23.7 MB (10.40%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n = s.size();
-        string res = "";
-        string word = "";
-        for (int i = n - 1; i >= 0; i--) {
-            if (s[i] != ' ') {
-                word += s[i];
-            }
-
-            else {
-                if (!word.empty()) {
-                    reverse(word.begin(), word.end());
-                    res += word + ' ';
-                    word = "";
-                }
-            }
-        }
-        if (!word.empty()) {
-            reverse(word.begin(), word.end());
-            res += word;
+        stringstream ss(s);
+        string res = "", word = "";
+        while (ss >> word) {
+            res = word + " " + res;
         }
         if (res.back() == ' ') {
             res.pop_back();
