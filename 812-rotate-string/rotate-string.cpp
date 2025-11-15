e00d624ec047d56a365e233ac3f6e4f1cf47ba81
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-11-15 15:31:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (61.34%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         int n = s.size();
-        int m =goal.size();
-        if(n!=m)return false;
-        string doubled = s+s;
-        return doubled.find(goal)!=string::npos;
+        int m = goal.size();
+        if (n != m)
+            return false;
+        string check = goal + goal;
+        return check.find(s) != -1;
     }
 };
