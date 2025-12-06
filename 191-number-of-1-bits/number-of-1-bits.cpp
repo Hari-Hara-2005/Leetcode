746e925488c4d2292712c076095e44b405af326d
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-12-06 17:58:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (12.64%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int hammingWeight(int n) {
         string res = "";
-        while (n > 0) {
-            if (n % 2 == 1)
-                res += '1';
-            else
-                res += '0';
-            n = n / 2;
-        }
         int cnt = 0;
-        for (int i = 0; i < res.size(); i++) {
-            if (res[i] == '1')
+        while (n > 0) {
+            if (n % 2 == 1) {
+                res += '1';
                 cnt++;
+            } else {
+                res += '0';
+            }
+            n = n / 2;
         }
         return cnt;
     }
