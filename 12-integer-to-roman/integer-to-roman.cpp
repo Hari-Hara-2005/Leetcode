fd4aa63813e8b572cca50b7d75eaf151437e429e
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-12-08 20:37:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (68.54%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        string res = "";
-        static string symbols[] = {"M",  "CM", "D",  "CD", "C",  "XC", "L",
-                                   "XL", "X",  "IX", "V",  "IV", "I"};
-        static int val[] = {1000, 900, 500, 400, 100, 90, 50,
-                            40,   10,  9,   5,   4,   1};
-        for (int i = 0; i < 13; i++) {
-            if (num == 0)
-                break;
-            int times = num / val[i];
-            while (times--) {
-                res += symbols[i];
-            }
-            num = num % val[i];
+      static vector<int> val = {1000,900,500,400,100,90,50,40,10,9,5,4,1};  
+      static vector<string> sym = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+      string res="";
+      for(int i=0;i<13;i++)
+      {
+        int times = num/val[i];
+        while(times--)
+        {
+            res+=sym[i];
         }
-        return res;
+        num = num%val[i];
+      }
+      return res;
     }
 };
