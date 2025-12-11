4e91cadd1c8b8ccc8ea0755cf829a578ccc6dc72
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-12-11 20:57:13 +0530

    Time: 646 ms (5.17%) | Memory: 394.6 MB (5.02%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    int getMaximum(vector<int>freq)
-    {
+    int getMaximum(vector<int> freq) {
         int maxi = INT_MIN;
         int mini = INT_MAX;
-        for(int i=0;i<26;i++)
-        {
-            maxi = max(freq[i],maxi);
-            if(freq[i] != 0)mini = min(freq[i],mini);
+        for (int i = 0; i < 26; i++) {
+            maxi = max(freq[i], maxi);
+            if (freq[i] != 0)
+                mini = min(freq[i], mini);
         }
-        if(maxi == INT_MIN || mini == INT_MAX)return 0;
+        if (maxi == INT_MIN || mini == INT_MAX)
+            return 0;
         return (maxi - mini);
     }
     int beautySum(string s) {
         int n = s.size();
-        int sum =0;
-        for(int i=0;i<n;i++)
-        {
-            vector<int>freq(26,0);
-            for(int j=i;j<n;j++)
-            {
-                freq[s[j]-'a']++;
-                sum+=getMaximum(freq);
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            vector<int> freq(26, 0);
+            for (int j = i; j < n; j++) {
+                freq[s[j] - 'a']++;
+                sum += getMaximum(freq);
             }
         }
         return sum;
