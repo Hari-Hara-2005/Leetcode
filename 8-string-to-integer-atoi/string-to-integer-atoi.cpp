89239f7b20d1d5dde21d6787ee783efcfe72d2db
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-12-11 18:56:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (54.96%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -7,14 +7,14 @@ public:
         long long res = 0;
         while (i < n && s[i] == ' ')
             i++;
-        if (i<n && (s[i] == '-' || s[i] == '+')) {
-            if(s[i]=='-')
-            sign = true;
+        if (i < n && (s[i] == '-' || s[i] == '+')) {
+            if (s[i] == '-')
+                sign = true;
             i++;
         }
         while (i < n && isdigit(s[i])) {
             res = res * 10 + (s[i] - '0');
-            if (sign && (res*-1)<= INT_MIN)
+            if (sign && (res * -1) <= INT_MIN)
                 return INT_MIN;
             if (!sign && res >= INT_MAX)
                 return INT_MAX;
