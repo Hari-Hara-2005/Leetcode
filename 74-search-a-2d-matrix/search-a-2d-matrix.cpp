8c9476c9575640629e86360444817571e3542248
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2025-12-26 10:54:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (41.47%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-      int n = matrix.size();
-      for(int i=0;i<n;i++)
-      {
-        int low =0 ;
-        int high = matrix[i].size()-1;
-        while(low<=high)
-        {
-            int mid = low + (high - low )/2;
-            if(matrix[i][mid] == target)
-            {
-                return true;
-            }
-            else if(matrix[i][mid]>target)
-            {
-                high = mid -1;
-            }
-            else{
-                low = mid +1;
+    bool searchMatrix(vector<vector<int>>& arr, int target) {
+        int n = arr.size();
+        int m = arr[0].size();
+        for (int i = 0; i < n; i++) {
+            int low = 0;
+            int high = m - 1;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (arr[i][mid] == target)
+                    return true;
+                else if (arr[i][mid] > target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
             }
         }
-      }  
-      return false;
+        return false;
     }
 };
