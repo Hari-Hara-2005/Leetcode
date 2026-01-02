849a1ca3e179aeeeff465d30d620bc32d3b10ab8
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-01-02 15:17:23 +0530

    Time: 20 ms (6.88%) | Memory: 28.1 MB (5.00%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int sum1=n*(n+1)/2;
-        int sum2=0;
-        for(int i=0;i<n;i++)
-        {
-            sum2+=nums[i];
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]]++;
         }
-        return (sum1-sum2);
+        for (int i = 0; i <= n; i++) {
+            if (mp[i] == 0)
+                return i;
+        }
+        return -1;
     }
 };
