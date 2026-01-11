2d172efa1ad752f1e5166e08c830ebbb6cf99d6b
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-01-11 11:38:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (62.84%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    vector<int> findPascal(int row)
-    {
-        vector<int>ans;
-        int res=1;
+    vector<int> findNcr(int n) {
+        vector<int> ans;
+        int res = 1;
         ans.push_back(res);
-        for(int i=1;i<row;i++)
-        {
-            res = res * (row-i);
+        for (int i = 1; i < n; i++) {
+            res = res * (n - i);
             res = res / i;
             ans.push_back(res);
         }
         return ans;
     }
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>ans;
-        for(int i=0;i<numRows;i++)
-        {
-            ans.push_back(findPascal(i+1));
+        vector<vector<int>> ans;
+        for (int i = 0; i < numRows; i++) {
+            ans.push_back(findNcr(i + 1));
         }
         return ans;
     }
