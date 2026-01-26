014ae483ce8ce91fe46723e8b9adbc57c7fc9abc
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-01-26 10:32:36 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (41.49%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& arr, int target) {
+        int low = 0;
+        int n = arr.size();
+        int high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
