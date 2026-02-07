0cc5a392716b1a4adea8c2bb3d8c1e92481f7026
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-02-07 16:59:49 +0530

    Time: 0 ms (100.00%) | Memory: 35.5 MB (29.42%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -17,8 +17,11 @@ public:
             return nullptr;
         if (val == root->val) {
             return root;
+        } else if (val > root->val) {
+            return searchBST(root->right, val);
+        } else if (val < root->val) {
+            return searchBST(root->left, val);
         }
-        return val > root->val ? searchBST(root->right, val)
-                               : searchBST(root->left, val);
+        return nullptr;
     }
 };
