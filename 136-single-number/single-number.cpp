4f8362f6868a9b94f4bb28cfc2220c00e43f57a3
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-06-27 12:39:30 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (58.08%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int, int> mp;
+        int result = 0;
         int n = nums.size();
         for (int i = 0; i < n; i++) {
-            mp[nums[i]]++;
+            result = result ^ nums[i];
         }
-        for (int i = 0; i < n; i++) {
-            if (mp[nums[i]] == 1) {
-                return nums[i];
-            }
-        }
-        return -1;
+        return result;
     }
 };
