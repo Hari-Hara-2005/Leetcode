1436513d11f170fd5d43d1a5d8267eb3eda3a5c7
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-06-30 11:00:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (63.42%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    vector<int> findNcr(int row) {
-        int res = 1;
+    vector<int> findCnr(int r) {
         vector<int> ans;
+        int res = 1;
         ans.push_back(res);
-        for (int i = 1; i < row; i++) {
-            res = res * (row - i);
+        for (int i = 1; i < r; i++) {
+            res = res * (r - i);
             res = res / i;
             ans.push_back(res);
         }
         return ans;
     }
+
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
         for (int i = 1; i <= numRows; i++) {
-            ans.push_back(findNcr(i));
+            ans.push_back(findCnr(i));
         }
         return ans;
     }
