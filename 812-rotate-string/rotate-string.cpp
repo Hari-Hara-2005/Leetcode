1b49d88221d6675625dc28ecc258efc624b49a5a
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-07-01 13:31:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (58.59%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -3,9 +3,8 @@ public:
     bool rotateString(string s, string goal) {
         int n = s.size();
         int m = goal.size();
-        if (n != m)
-            return false;
-        string check = goal + goal;
-        return check.find(s) != -1;
+        string doubleStr = goal+goal;
+        if(n!=m)return false;
+        return doubleStr.find(s) != -1 ;
     }
 };
