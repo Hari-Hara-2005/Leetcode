dcd78dc8b797610b6dd53e283f3a833133be05e2
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-07-01 14:57:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (85.50%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        int maxi = 0;
-        int cnt = 0;
         int n = s.size();
+        int maxi = INT_MIN;
+        int cnt = 0;
         for (int i = 0; i < n; i++) {
             if (s[i] == '(') {
                 cnt++;
-                maxi = max(maxi, cnt);
             } else if (s[i] == ')') {
                 cnt--;
             }
+            maxi = max(cnt, maxi);
         }
         return maxi;
     }
