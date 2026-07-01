b9cedc4c8429a31247e5711dcce8913af1ec8df2
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-07-01 15:20:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (94.19%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    int num(char ch)
-    {
-        if(ch == 'I')return 1;
-        else if(ch == 'V')return 5;
-        else if(ch=='X')return 10;
-        else if(ch=='L')return 50;
-        else if(ch =='C')return 100;
-        else if(ch == 'D')return 500;
-        else if(ch == 'M')return 1000;
-        return 0;
+    int num(char ch) {
+        if (ch == 'I')
+            return 1;
+        else if (ch == 'V')
+            return 5;
+        else if (ch == 'X')
+            return 10;
+        else if (ch == 'L')
+            return 50;
+        else if (ch == 'C')
+            return 100;
+        else if (ch == 'D')
+            return 500;
+        else if (ch == 'M')
+            return 1000;
+        else
+            return -1;
     }
     int romanToInt(string s) {
-        int n =s.size();
-        int res=0;
-        for(int i=0;i<n-1;i++)
-        {
-            int v1 = num(s[i]);
-            int v2 = num(s[i+1]);
-            if(v1>=v2)
-            {
-                res+=v1;
-            }
-            else{
-                res-=v1;
+        int res = 0;
+        int n = s.size();
+        for (int i = 0; i < n - 1; i++) {
+            int n1 = num(s[i]);
+            int n2 = num(s[i + 1]);
+            if (n1 < n2) {
+                res -= n1;
+            } else {
+                res += n1;
             }
         }
-        res+=num(s[n-1]);
+        res += num(s[n - 1]);
         return res;
     }
 };
