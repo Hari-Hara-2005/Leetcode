646a18aef7c5b2285bee8581b4f57ace6aebb1c7
Hari Haran <160337844+Hari-Hara-2005@users.noreply.github.com>
2026-07-02 11:45:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (21.82%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -2,24 +2,33 @@ class Solution {
 public:
     int myAtoi(string s) {
         int n = s.size();
+        long long res = 0;
         int i = 0;
         bool sign = false;
-        long long res = 0;
-        while (i < n && s[i] == ' ')
-            i++;
-        if (i < n && (s[i] == '-' || s[i] == '+')) {
-            if (s[i] == '-')
-                sign = true;
+        while (i < n && s[i] == ' ') {
             i++;
         }
+
+        if (s[i] == '-' || s[i] == '+') {
+            if (s[i] == '-') {
+                sign = true;
+            }
+            i++;
+        }
+
         while (i < n && isdigit(s[i])) {
             res = res * 10 + (s[i] - '0');
-            if (sign && (res * -1) <= INT_MIN)
+            if (sign && (res * -1) <= INT_MIN) {
                 return INT_MIN;
-            if (!sign && res >= INT_MAX)
+            }
+            if (!sign && res >= INT_MAX) {
                 return INT_MAX;
+            }
             i++;
         }
-        return sign ? (-1 * res) : res;
+        if (sign) {
+            res = res * -1;
+        }
+        return res;
     }
 };
